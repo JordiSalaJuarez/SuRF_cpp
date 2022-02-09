6f44aa0220ba48a2b6ed49066711d5b7fe667eb0
JordiSalaJuarez <jordisalajuarez@gmail.com>
2022-02-09 18:49:53 +0100

    Added prefix keys, tree prunning , complex trie tests and fixed bugs with Complex tries

--- surf/surf.h
+++ surf/surf.h
@@ -2,48 +2,102 @@
 #include<bitset>
 // #include<iterator>
 #include <algorithm>
-using std::vector;
 
-
+using namespace std;
 
 struct LoudsBuilder {
     public:
-    vector<vector<char>> labels;
-    vector<vector<bool>> has_child;
-    vector<vector<bool>> louds;
-    size_t n_levels;
+    vector<vector<char>> labels{};
+    vector<vector<bool>> has_child{};
+    vector<vector<bool>> louds{};
+    vector<vector<bool>> is_prefix{};
+    vector<vector<string>> suffix{};
+    size_t n_levels{};
 
-    static LoudsBuilder from_stream(std::istream &input){
+    static LoudsBuilder from_stream(istream &input){
 
         LoudsBuilder result{};
         auto &labels = result.labels;
         auto &has_child = result.has_child;
         auto &louds = result.louds;
         auto &n_levels = result.n_levels;
+        auto &suffix = result.suffix;
+        auto &is_prefix = result.is_prefix;
 
-        for (std::string word = {}; std::getline(input, word);) {
-			if (size(word) > size(labels)) {
-				resize<char>(labels, size(word));
-				resize<bool>(has_child, size(word));
-				resize<bool>(louds, size(word));
-			}
-				
-			auto lca = lowest_common_ancestor(labels, word);
-			auto level = lca;
-			while (level < size(word)-1) {
-				labels[level].emplace_back(word[level]);
+
+        auto insert_key = [&](const string& key, size_t start, size_t end){
+            if (end+1 > n_levels){
+                n_levels = end+1;
+                resize<char>(labels, n_levels);
+				resize<bool>(has_child, n_levels);
+				resize<bool>(louds, n_levels);
+                resize<bool>(is_prefix, n_levels);
+                resize<string>(suffix, n_levels);
+            }
+            for (auto level = start; level < end; level++) { // insert new non leaf and nodes 
+				labels[level].emplace_back(key[level]);
 				has_child[level].emplace_back(true);
-				louds[level].emplace_back(lca != level);
-				level++;
+                is_prefix[level].emplace_back(false);
+				louds[level].emplace_back(start != level);
 			}
-			labels[level].emplace_back(word[level]);
-			has_child[level].emplace_back(false);
-			louds[level].emplace_back(lca != level);
-		}
-		if (size(louds) > 0 && size(louds[0]) > 0)
-			louds[0][0] = true; // Root of tree always has bit set to 1
+            if(end < size(key)){
+                // insert new leaf node
+                labels[end].emplace_back(key[end]);
+                has_child[end].emplace_back(false);
+                is_prefix[end].emplace_back(false);
+                louds[end].emplace_back(start != end);
+                // store suffix
+                suffix[end].emplace_back(key.substr(end+1));
+            } else {
+                labels[end].emplace_back('$');
+                has_child[end].emplace_back(false);
+                is_prefix[end].emplace_back(true);
+                louds[end].emplace_back(start != end);
+                suffix[end].emplace_back("");
+            }
+        };
 
-        n_levels = size(labels);
+
+        auto add_dollar = [&](const auto& i){
+            if (i+2 > n_levels){
+                n_levels = i+2;
+                resize<char>(labels, n_levels);
+				resize<bool>(has_child, n_levels);
+				resize<bool>(louds, n_levels);
+            }
+            has_child[i][size(has_child[i])] = true;
+            labels[i+1].emplace_back('$');
+            louds[i+1].emplace_back(true);
+            has_child[i+1].emplace_back(false);
+        };
+
+
+        auto lowest_common_ancestor_prev = [&](const auto& x){
+            auto i = 0;
+            while(i < size(x) && i < n_levels && x[i] == labels[i].back() && has_child[i].back()) i++; // treat antecesors
+            if(i < size(x) && i < n_levels && x[i] == labels[i].back()){ // treat leaf
+                add_dollar(i);
+                i++; 
+            }
+            return i;
+        };
+        auto lowest_common_ancestor_next = [&](const auto& x, const auto& y){
+            auto i = 0;
+            while(i < size(x) && i < size(y) && x[i] == y[i]) i++;
+            return i;
+        };
+
+        string key{}, next_key{};
+        if (!getline(input, key)) return result;
+        while(getline(input, next_key)){
+            auto lca_prev = lowest_common_ancestor_prev(key);
+            auto lca_next = lowest_common_ancestor_next(key, next_key);
+            insert_key(key, lca_prev, max(lca_prev, lca_next));
+            key = next_key; // is this going to be a copy? move?
+		}
+        auto lca_prev = lowest_common_ancestor_prev(key);
+        insert_key(key, lca_prev, lca_prev);
+        louds[0][0] = true;
 
         return result;
     }
@@ -64,16 +118,24 @@ struct LoudsBuilder {
 
 template<typename T>
 struct LoudsDense {
-    vector<std::bitset<256>> labels;
-    vector<std::bitset<256>> has_child;
-    vector<bool> is_prefix_key;
-    vector<T> values;
+    vector<std::bitset<256>> labels{};
+    vector<std::bitset<256>> has_child{};
+    vector<bool> is_prefix_key{};
+    vector<string> values{};
+
+    static void merge_and_insert(const auto & vs, auto &r, size_t capacity){
+        r.reserve(capacity);
+        for (const auto &v: vs) r.insert(end(r), begin(v), end(v)); 
+    }
 
     static LoudsDense from_builder(LoudsBuilder &builder){
         LoudsDense result{};
         auto vs = builder.louds;
         auto sum_size = 0;
         for (const auto &v: vs) sum_size += std::count(begin(v), end(v), true);
+        auto sum_size_values = 0;
+        for (const auto &v: builder.suffix) sum_size_values += size(v);
+        merge_and_insert(builder.suffix, result.values, sum_size_values);  
         result.labels.reserve(sum_size);
         result.has_child.reserve(sum_size);
         result.is_prefix_key.reserve(sum_size);
@@ -84,10 +146,11 @@ struct LoudsDense {
                 if (builder.louds[level][i]){
                     result.labels.emplace_back();
                     result.has_child.emplace_back();
-                    result.is_prefix_key.emplace_back();
+                    result.is_prefix_key.emplace_back(builder.is_prefix[level][i]);
                 }
-                result.labels.back().set(builder.labels[level][i], true);
-                result.has_child.back().set(builder.labels[level][i], builder.has_child[level][i]);
+                auto c = builder.labels[level][i];
+                result.labels.back().set(c, true);
+                result.has_child.back().set(c, builder.has_child[level][i]);
             }
         }
         return result;
@@ -144,19 +207,20 @@ struct LoudsDense {
     }
 
     size_t value(size_t pos){
-        return rank_l(pos) - rank_c(pos) + rank_p(pos);
+        return rank_l(pos) - rank_c(pos) + rank_p(pos/256) - 1;
     }
 
 
 
     bool look_up(const std::string& word) { 
         for (auto level=0, pos=-1; level < size(word); level++){
-            if (labels[child(pos)/256][word[level]]) 
-                if (has_child[child(pos)/256][word[level]])
-                    pos = child(pos) + word[level];
-                else
-                    return size(word) == level+1;
-            else
+            pos = child(pos) + word[level];
+            if (labels[pos/256][word[level]]) {
+                if (!has_child[pos/256][word[level]]){
+                    auto suffix = values[value(pos)];
+                    return word.compare(level+1, size(suffix), suffix) == 0; // chech if the suffix is the same
+                }
+            } else
                 return false;
         }
 		return false;
@@ -171,7 +235,7 @@ struct LoudsSparse {
     vector<char> labels;
     vector<bool> has_child;
     vector<bool> louds;
-    vector<T> values;
+    vector<string> values;
 
 
     // decltype(this)
@@ -182,6 +246,9 @@ struct LoudsSparse {
         merge_and_insert(builder.labels, result.labels, sum_size);
         merge_and_insert(builder.has_child, result.has_child, sum_size);
         merge_and_insert(builder.louds, result.louds, sum_size);
+        auto sum_size_values = 0;
+        for (const auto &v: builder.suffix) sum_size_values += size(v);
+        merge_and_insert(builder.suffix, result.values, sum_size_values);
         return result;
     }
 
@@ -239,7 +306,10 @@ struct LoudsSparse {
         for (auto level=0, pos=-1; level < size(word); level++){
             pos = find(child_begin(pos), child_end(pos), word[level]);
 			if (pos == -1) return false; // failed to find char
-            else if (!has_child[pos]) return size(word) == level+1; // last char found
+            else if (!has_child[pos]){
+                auto suffix = values[value(pos)];
+                return word.compare(level+1, size(suffix), suffix) == 0; // chech if the suffix is the same
+            }
         }
 		return false;
 	}
--- surf/test.cpp
+++ surf/test.cpp
@@ -5,7 +5,7 @@
 #include "utils.h"
 #include <span>
 
-
+using namespace std;
 // Demonstrate some basic assertions.
 TEST(LoudsBuilderTest, PrimitiveTrie) {
   // using namespace std::string_literals;
@@ -16,23 +16,71 @@ TEST(LoudsBuilderTest, PrimitiveTrie) {
   iss << "aca" << std::endl;
 
   auto n_levels = 3;
-  std::vector<std::vector<char>> labels =     {{'a'}, {'a', 'b', 'c'}, {'a', 'a', 'b', 'a'}};
-  std::vector<std::vector<bool>> has_child =  {{ 1 }, { 1,   1,   1 }, { 0,   0,   0,   0 }};
-  std::vector<std::vector<bool>> louds =      {{ 1 }, { 1,   0,   0 }, { 1,   1,   0,   1 }};
+  vector<vector<char>> labels =     {{'a'}, {'a', 'b', 'c'}, {'a', 'b'}};
+  vector<vector<bool>> has_child =  {{ 1 }, { 0,   1,   0 }, { 0,   0 }};
+  vector<vector<bool>> louds =      {{ 1 }, { 1,   0,   0 }, { 1,   0 }};
+  vector<vector<string>> suffix =   {{}, {"a", "a"}, {"", ""}};
   auto builder = LoudsBuilder::from_stream(iss);
   ASSERT_EQ(builder.n_levels, n_levels) << "Number of levels is wrong, expected " << n_levels;
   ASSERT_TRUE(eq(size(builder.labels),size(builder.has_child), size(builder.louds), size(labels))) << "Not all sizes are the same";
   for(auto i : {0,1,2}){
     ASSERT_EQ(size(builder.labels[i]), size(labels[i])) << "nested labels size is different at level" << i;
     for (auto j=0; j < size(builder.labels[i]); j++){
-      ASSERT_EQ(builder.labels[i][j], labels[i][j]) << "different labels element found at level " << i << " position " << j;
+      EXPECT_EQ(builder.labels[i][j], labels[i][j]) << "different labels element found at level " << i << " position " << j;
     }
     ASSERT_EQ(size(builder.has_child[i]), size(has_child[i]))  << "nested has_child size is different at level " << i;
     for (auto j=0; j < size(builder.has_child[i]); j++)
-      ASSERT_EQ(builder.has_child[i][j], has_child[i][j]) << "different labels element found at level " << i << " position " << j;
+      EXPECT_EQ(builder.has_child[i][j], has_child[i][j]) << "different labels element found at level " << i << " position " << j;
     ASSERT_EQ(size(builder.louds[i]), size(louds[i])) << "nested louds size is different at level " << i;
     for (auto j=0; j < size(builder.louds[i]); j++)
-      ASSERT_EQ(builder.louds[i][j], louds[i][j]) << "different louds element found at level " << i << " position " << j; 
+      EXPECT_EQ(builder.louds[i][j], louds[i][j]) << "different louds element found at level " << i << " position " << j; 
+    ASSERT_EQ(size(builder.suffix[i]), size(suffix[i])) << "nested suffix size is different at level " << i;
+    for (auto j=0; j < size(builder.suffix[i]); j++)
+      EXPECT_EQ(builder.suffix[i][j], suffix[i][j]) << "different suffixes found at level " << i << "position " << j << " where " << builder.suffix[i][j] << " != " << suffix[i][j];
+  }
+ 
+}
+
+
+
+TEST(LoudsBuilderTest, ComplexTrie) {
+  // using namespace std::string_literals;
+  std::stringstream iss;
+  iss << "f" << std::endl;
+  iss << "farther" << std::endl;
+  iss << "fas" << std::endl;
+  iss << "fasten" << std::endl;
+  iss << "fat" << std::endl;
+  iss << "splice" << std::endl;
+  iss << "topper" << std::endl;
+  iss << "toy" << std::endl;
+  iss << "tries" << std::endl;
+  iss << "tripper" << std::endl;
+  iss << "trying" << std::endl;
+
+  auto n_levels = 4;
+  std::vector<std::vector<char>> labels =     {{'f', 's', 't'}, {'$', 'a', 'o', 'r'}, {'r', 's', 't', 'p', 'y', 'i', 'y'}, {'$', 't', 'e', 'p'}};
+  std::vector<std::vector<bool>> has_child =  {{ 1 ,  0 ,  1 }, { 0 ,  1 ,  1 ,  1 }, { 0 ,  1 ,  0 ,  0 ,  0 ,  1 ,  0 }, { 0 ,  0 ,  0 ,  0 }};
+  std::vector<std::vector<bool>> louds =      {{ 1 ,  0 ,  0 }, { 1 ,  0 ,  1 ,  0 }, { 1 ,  0 ,  0 ,  1 ,  0 ,  1 ,  0 }, { 1 ,  0 ,  1 ,  0 }};
+  vector<vector<string>> suffix = {{"plice"}, {""}, {"ther", "", "per", "", "ing"}, {"", "en", "s", "per"}};
+  auto builder = LoudsBuilder::from_stream(iss);
+  ASSERT_EQ(builder.n_levels, n_levels) << "Number of levels is wrong, expected " << n_levels;
+  ASSERT_TRUE(eq(size(builder.labels),size(builder.has_child), size(builder.louds), size(labels))) << "Not all sizes are the same";
+  for(auto i : {0,1,2,3}){
+    ASSERT_EQ(size(builder.labels[i]), size(labels[i])) << "nested labels size is different at level" << i;
+    for (auto j=0; j < size(builder.labels[i]); j++){
+      EXPECT_EQ(builder.labels[i][j], labels[i][j]) << "different labels element found at level " << i << " position " << j;
+    }
+    ASSERT_EQ(size(builder.has_child[i]), size(has_child[i]))  << "nested has_child size is different at level " << i;
+    for (auto j=0; j < size(builder.has_child[i]); j++)
+      EXPECT_EQ(builder.has_child[i][j], has_child[i][j]) << "different labels element found at level " << i << " position " << j;
+    ASSERT_EQ(size(builder.louds[i]), size(louds[i])) << "nested louds size is different at level " << i;
+    for (auto j=0; j < size(builder.louds[i]); j++)
+      EXPECT_EQ(builder.louds[i][j], louds[i][j]) << "different louds element found at level " << i << " position " << j; 
+    ASSERT_EQ(size(builder.suffix[i]), size(suffix[i])) << "nested suffix size is different at level " << i;
+    for (auto j=0; j < size(builder.suffix[i]); j++)
+      EXPECT_EQ(builder.suffix[i][j], suffix[i][j]) << "different suffixes found at level " << i << "position " << j << " where " << builder.suffix[i][j] << " != " << suffix[i][j];
+
   }
 }
 
@@ -46,34 +94,41 @@ TEST(LoudsSparseTest, PrimitiveTrie) {
   iss << "aca" << std::endl;
 
   auto n_levels = 3;
-  std::vector<char> labels =     {'a', 'a', 'b', 'c', 'a', 'a', 'b', 'a'};
-  std::vector<bool> has_child =  { 1,   1,   1,   1,   0,   0,   0,   0 };
-  std::vector<bool> louds =      { 1 ,  1,   0,   0,   1,   1,   0,   1 };
+  std::vector<char> labels =     {'a', 'a', 'b', 'c', 'a', 'b'};
+  std::vector<bool> has_child =  { 1 ,  0,   1,   0 ,  0,   0 };
+  std::vector<bool> louds =      { 1 ,  1,   0,   0 ,  1,   0 };
+  vector<string> suffix =        { "a", "a", "", ""};
+
   auto builder = LoudsBuilder::from_stream(iss);
   auto sparse = LoudsSparse<char>::from_builder(builder);
   ASSERT_EQ(builder.n_levels, n_levels) << "Number of levels is wrong, expected " << n_levels;
   ASSERT_TRUE(eq(size(sparse.labels),size(sparse.has_child), size(sparse.louds), size(labels))) << "Not all sizes are the same";
   ASSERT_EQ(size(sparse.labels), size(labels));
   for (auto j=0; j < size(sparse.labels); j++){
-    ASSERT_EQ(sparse.labels[j], labels[j]) << "different labels element found at position " << j;
+    EXPECT_EQ(sparse.labels[j], labels[j]) << "different labels element found at position " << j;
   }
   ASSERT_EQ(size(sparse.has_child), size(has_child));
   for (auto j=0; j < size(sparse.has_child); j++)
-    ASSERT_EQ(sparse.has_child[j], has_child[j]) << "different labels element found at level position " << j;
+    EXPECT_EQ(sparse.has_child[j], has_child[j]) << "different labels element found at level position " << j;
   ASSERT_EQ(size(sparse.louds), size(louds));
   for (auto j=0; j < size(sparse.louds); j++)
-    ASSERT_EQ(sparse.louds[j], louds[j]) << "different louds element found at level position " << j; 
+    EXPECT_EQ(sparse.louds[j], louds[j]) << "different louds element found at level position " << j; 
+
+  ASSERT_EQ(size(sparse.values), size(suffix));
+  for (auto j=0; j < size(sparse.values); j++)
+    EXPECT_EQ(sparse.values[j], suffix[j]) << "different values found at level position " << j; 
   
-  ASSERT_TRUE(sparse.look_up("aaa"));
-  ASSERT_TRUE(sparse.look_up("aba"));
-  ASSERT_TRUE(sparse.look_up("abb"));
-  ASSERT_TRUE(sparse.look_up("aca"));
-  ASSERT_FALSE(sparse.look_up("acb"));
-  ASSERT_FALSE(sparse.look_up("bca"));
-  ASSERT_FALSE(sparse.look_up("bcad"));
-  ASSERT_FALSE(sparse.look_up("dcae"));
+  EXPECT_TRUE(sparse.look_up("aaa"));
+  EXPECT_TRUE(sparse.look_up("aba"));
+  EXPECT_TRUE(sparse.look_up("abb"));
+  EXPECT_TRUE(sparse.look_up("aca"));
+  EXPECT_FALSE(sparse.look_up("acb"));
+  EXPECT_FALSE(sparse.look_up("bca"));
+  EXPECT_FALSE(sparse.look_up("bcad"));
+  EXPECT_FALSE(sparse.look_up("dcae"));
   
 }
+
 
 // lets you create a bitstring containing chars 'a' 'b' as following "ab"bs
 constexpr std::bitset<256> operator""_bs(const char *str, std::size_t len){
@@ -94,26 +149,31 @@ TEST(LoudsDenseTest, PrimitiveTrie) {
   iss << "aca" << std::endl;
 
   auto n_levels = 3;
-  vector<std::bitset<256>> labels =     {"a"_bs, "abc"_bs, "a"_bs, "ab"_bs, "a"_bs};
-  vector<std::bitset<256>> has_child =  {"a"_bs, "abc"_bs,  ""_bs,   ""_bs,  ""_bs};
+  vector<std::bitset<256>> labels =     {"a"_bs, "abc"_bs, "ab"_bs };
+  vector<std::bitset<256>> has_child =  {"a"_bs,  "b"_bs,  ""_bs };
+  vector<string> suffix =               { "a", "a", "", ""};
   auto builder = LoudsBuilder::from_stream(iss);
   auto dense = LoudsDense<char>::from_builder(builder);
   ASSERT_EQ(builder.n_levels, n_levels) << "Number of levels is wrong, expected " << n_levels;
   ASSERT_TRUE(eq(size(dense.labels),size(dense.has_child), size(labels))) << "Not all sizes are the same";
   ASSERT_EQ(size(dense.labels), size(labels));
   for (auto j=0; j < size(dense.labels); j++){
-    ASSERT_EQ(dense.labels[j], labels[j]) << "different labels element found at position " << j;
+    EXPECT_EQ(dense.labels[j], labels[j]) << "different labels element found at position " << j;
   }
   ASSERT_EQ(size(dense.has_child), size(has_child));
   for (auto j=0; j < size(dense.has_child); j++)
-    ASSERT_EQ(dense.has_child[j], has_child[j]) << "different labels element found at level position " << j;
+    EXPECT_EQ(dense.has_child[j], has_child[j]) << "different labels element found at level position " << j;
   
-  ASSERT_TRUE(dense.look_up("aaa"));
-  ASSERT_TRUE(dense.look_up("aba"));
-  ASSERT_TRUE(dense.look_up("abb"));
-  ASSERT_TRUE(dense.look_up("aca"));
-  ASSERT_FALSE(dense.look_up("acb"));
-  ASSERT_FALSE(dense.look_up("bca"));
-  ASSERT_FALSE(dense.look_up("bcad"));
-  ASSERT_FALSE(dense.look_up("dcae"));
+  ASSERT_EQ(size(dense.values), size(suffix));
+  for (auto j=0; j < size(dense.values); j++)
+    EXPECT_EQ(dense.values[j], suffix[j]) << "different values found at level position " << j; 
+
+  EXPECT_TRUE(dense.look_up("aaa"));
+  EXPECT_TRUE(dense.look_up("aba"));
+  EXPECT_TRUE(dense.look_up("abb"));
+  EXPECT_TRUE(dense.look_up("aca"));
+  EXPECT_FALSE(dense.look_up("acb"));
+  EXPECT_FALSE(dense.look_up("bca"));
+  EXPECT_FALSE(dense.look_up("bcad"));
+  EXPECT_FALSE(dense.look_up("dcae"));
 }
